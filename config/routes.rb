f53139c3301c4793f37af82643006da8dd784d7f
Chonwai <chonwaiun@gmail.com>
2024-11-07 17:53:25 +0800

    Add: Added CORS configuration and update routes.rb
    This commit refactors the CORS configuration in the `config/initializers/cors.rb` file. It updates the allowed origins to include the `FRONTEND_URL` environment variable and `http://localhost:3001`. Additionally, it adds the `credentials: true` option to allow cross-origin requests with credentials.
    
    In the `config/routes.rb` file, the commit updates the routes for the API version 1. It adds nested resources for activities, locations, and temp_users. It also adds a member route for converting a temp_user to a user. Furthermore, it adds routes for check_ins, rewards, and users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :activities do
+        resources :locations
+        resources :temp_users do
+          member do
+            post :convert_to_user
+          end
+        end
+      end
+      
+      resources :check_ins, only: [:create, :index]
+      resources :rewards, only: [:index, :show]
+      resources :users, only: [:create, :show, :update]
+    end
+  end
 end
