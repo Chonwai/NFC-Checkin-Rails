8c848a883666de41988a185da6c64a2c77ad5c0a
Chonwai <chonwaiun@gmail.com>
2024-11-15 17:57:27 +0800

    Update: Update CORS configuration to allow requests from 'http://192.168.50.104:3000'

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://localhost:3000'
+    origins 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://192.168.50.104:3000'
 
     resource '*',
              headers: :any,
